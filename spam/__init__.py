24252f1958644dbd4c93b224a44f2aa273cccc87
Jochen Klar <mail@jochenklar.de>
2019-02-17 12:12:06 +0100

    Init repo

---
+++ spam/__init__.py
@@ -0,0 +1,8 @@
+__title__ = 'spam'
+__version__ = '0.1.0'
+__author__ = 'Jochen Klar'
+__email__ = 'mail@jochenklar.de'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Jochen Klar'
+
+VERSION = __version__
