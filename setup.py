dffb94c2c23fbb81c66634ec5339645031f013da
Jochen Klar <mail@jochenklar.de>
2021-12-20 10:21:08 +0100

    Move requirements to separate file

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ from setuptools import setup, find_packages
 with open('spam/__init__.py') as f:
     metadata = dict(re.findall(r'__(.*)__ = [\']([^\']*)[\']', f.read()))
 
+with open('requirements.txt') as f:
+    install_requires = f.readlines()
+
 setup(
     name=metadata['title'],
     version=metadata['version'],
@@ -16,14 +19,14 @@ setup(
     url='https://github.com/jochenklar/',
     description=u'',
     long_description=open('README.md').read(),
-    install_requires=[
-        'requests>=2.21.0',
-    ],
+    install_requires=install_requires,
     classifiers=[
         # https://pypi.org/classifiers/
         'Development Status :: 1 - Planning',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Operating System :: OS Independent',
         'License :: OSI Approved :: MIT License',
     ],
